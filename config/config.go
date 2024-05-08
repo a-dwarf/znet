2ae538750a4149a44b20a6f0782374410726c730
a dwarf <weipinghang@gmail.com>
2024-05-08 20:30:02 +0800

    ci

--- config/config.go
+++ config/config.go
@@ -5,7 +5,7 @@ import "github.com/bufrr/znet/dht"
 const DEFAULT_P2P_PORT = 33333
 const DEFAULT_WS_PORT = 23333
 const DEFAULT_UDP_PORT = 8080
-const DEFAULT_VLC_ADDR = "127.0.0.1:8080"
+const DEFAULT_VLC_ADDR = "192.168.50.209:8080"
 
 type Config struct {
 	Transport string
